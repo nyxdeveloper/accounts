6321fe90d78a60ee0b81ca6827909c6345d089a7
nikolai <nikolai@itpw.ru>
2022-05-06 12:11:41 +0300

    Добавил авторизацию через почту

--- views.py
+++ views.py
@@ -21,7 +21,7 @@ class AuthenticationViewSet(GenericViewSet):
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserSerializer
 
-    def phone_auth(self, request):
+    def phone_auth_autoreg(self, request):
         phone = request.data.get("phone")
         otc = request.data.get("otc")
 
@@ -39,10 +39,28 @@ class AuthenticationViewSet(GenericViewSet):
             raise InvalidOTC
         return Response(send_otc(phone))
 
+    def email_auth(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not email:
+            return Response({"detail": "Укажите email"}, status=400)
+        if not password:
+            return Response({"detail": "Укажите пароль"}, status=400)
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return Response(get_auth_payload(user, request), status=200)
+
+        return Response({"detail": "Неверный логин или пароль"}, status=400)
+
     @transaction.atomic
     @action(methods=["POST"], detail=False)
     def auth(self, request):
         auth_type = request.query_params.get("t")
-        if auth_type == "phone":
-            return self.phone_auth(request)
+        if auth_type == "phone_autoreg":
+            return self.phone_auth_autoreg(request)
+        if auth_type == "email":
+            return self.email_auth(request)
         return Response({"detail": "Недоступный метод авторизации"}, status=400)
